88d29fecc868efa60b60dda562ffe12b0dee84eb
nisanurliah <adnis1002@gmail.com>
2024-09-07 19:40:23 +0700

    tambahan uas selesai

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,53 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('heading', 'Dashboard')
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+@section('title', 'Dashboard')
 
-        <!-- Styles -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@section('content')    
+    <div class="card-header">
 
-    </head>
-    <body class="container">
-        @if (session('success'))
-            <div class="alert alert-success mt-4">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        <div class="mt-4">
-            <h2>Data Penjualan Barang</h2>
-            <a href="/create" class="btn btn-primary mb-2">Tambah Data</a>
-            <table class="table table-responsive table-bordered">
-                <thead>
-                    <th>#</th>
-                    <th>Nama Barang</th>
-                    <th>Tanggal Penjualan</th>
-                    <th>Pelanggan</th>
-                    <th>Jumlah</th>
-                    <th>Harga Satuan</th>
-                    <th>Total</th>
-                </thead>
-                <tbody>
-                    @foreach ($penjualans as $penjualan)
-                        <tr>
-                            <td>{{ $penjualan->id }}</td>
-                            <td>{{ $penjualan->nama_barang }}</td>
-                            <td>{{ $penjualan->tanggal_penjualan }}</td>
-                            <td>{{ $penjualan->pelanggan }}</td>
-                            <td>{{ $penjualan->jumlah }}</td>
-                            <td>{{ $penjualan->harga_satuan }}</td>
-                            <td>{{ $penjualan->total }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </body>
-</html>
+    </div>
+    <div class="card-body">
+        <h1 class="text text-gray-500">Selamat Datang di Aplikasi Penjualan Barang</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PenjualanBarangController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FileUploadController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PenjualanBarangController::class, 'index']);
-Route::get('/create', [PenjualanBarangController::class, 'create']);
-Route::post('/store', [PenjualanBarangController::class, 'store']);
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login-form');
+Route::post('/login', [LoginController::class, 'login'])->name('login-post');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/', [DashboardController::class, 'index'])->name('welcome-page')->middleware(Authenticate::class);
+
+Route::get('/penjualan', [PenjualanBarangController::class, 'index'])->name("list-penjualan")->middleware(Authenticate::class);
+Route::get('/penjualan/create', [PenjualanBarangController::class, 'create'])->name("create-penjualan")->middleware(Authenticate::class);
+Route::post('/penjualan/store', [PenjualanBarangController::class, 'store'])->name("post-penjualan")->middleware(Authenticate::class);
+Route::get('/penjualan/{penjualanBarang}/edit', [PenjualanBarangController::class, 'edit'])->name("edit-penjualan")->middleware(Authenticate::class);
+Route::put('/penjualan/{penjualanBarang}/update', [PenjualanBarangController::class, 'update'])->name("update-penjualan")->middleware(Authenticate::class);
+Route::delete('/penjualan/{penjualanBarang}/delete', [PenjualanBarangController::class, 'delete'])->name("delete-penjualan")->middleware(Authenticate::class);
+
+Route::get('/profile', [FileUploadController::class, 'FormAvatar'])->name('profile')->middleware(Authenticate::class);
+Route::put('/profile/avatar/{user}', [FileUploadController::class, 'store'])->name('profile-store')->middleware(Authenticate::class);
